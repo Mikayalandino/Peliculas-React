463d376902eced7c811a6d5468705f8a3c484676
Mika <Micaelayalandino@gmail.com>
2022-04-02 22:27:32 -0300

    se agregó nav

---
+++ src/Components/Nav.js
@@ -0,0 +1,13 @@
+import '../Styles/Nav.css';
+
+const Nav = () => {
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+            </ul>
+        </nav>
+    )
+};
+
+export default Nav;
