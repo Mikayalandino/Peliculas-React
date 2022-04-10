a5ee371caa618f7867ef94baa833236bfddbea17
Mika <Micaelayalandino@gmail.com>
2022-04-10 19:15:23 -0300

    se agrego todos los ruteos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import './App.scss';
 import "./Styles/Reset.scss";
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import Nav from './Components/Nav';
+import Home from './Components/Home';
+import Movies from './Components/Movies';
+import Series from './Components/Series';
+import Search from './Components/Search';
+import Error404 from './Components/Error404';
 
 const App = () => {
   return (
-    <div>
+    <BrowserRouter>
       <Nav/>
-    </div>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/movies' element={<Movies />}></Route>
+        <Route path='/series' element={<Series />}></Route>
+        <Route path='/search' element= {<Search />}></Route>
+        <Route path='/*' element= {<Error404 />}></Route>
+      </Routes>
+      
+    </BrowserRouter>
     
   )
 };
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,17 +1,20 @@
 import '../Styles/Nav.scss';
 import { FiHome as HomeIcon } from "react-icons/fi";
-import { FiVideo as VideoIcon } from "react-icons/fi";
-import { FiTv as TvIcon } from "react-icons/fi";
+import { FiVideo as MovieIcon } from "react-icons/fi";
+import { FiTv as SerieIcon } from "react-icons/fi";
 import { FiSearch as SearchIcon } from "react-icons/fi";
 
 const Nav = () => {
     return (
         <nav>
             <ul>
-                <li><HomeIcon/></li>
-                <li><VideoIcon/></li>
-                <li><TvIcon/></li>
-                <li><input type="text"><SearchIcon/></input></li>
+                <li><HomeIcon /></li>
+                <li><MovieIcon /></li>
+                <li><SerieIcon /></li>
+                <label><SearchIcon/>
+                    <input type="text" />
+                </label>
+                
             </ul>
         </nav>
     )
