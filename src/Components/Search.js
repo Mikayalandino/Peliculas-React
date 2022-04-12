7301693fd8e62c4efed7859adefc7618bed28045
Mika <Micaelayalandino@gmail.com>
2022-04-12 20:36:15 -0300

    se agrego el useEffect y el map para los resultados

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,11 +1,31 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+
 const Search = () => {
+
+    const params = useParams()
+    const [searchResultados, setSearchResultados] = useState([])
+
+    useEffect(() => {
+        fetch(`https://api.themoviedb.org/3/search/multi?query=${params.title}`)
+        .then(res => res.json()
+        .then(data => {
+            setSearchResultados(data)
+        }))
+    }, [])
 
     return (
         <div>
             <h1>Resultados para: </h1>
+            {searchResultados.map(resultado => (
+                <h1 key={resultado.id}>{resultado.name}</h1>
+            ))
+            }
+
         </div>
-        
-    )    
+    )
+
 } 
 
 export default Search;
+
