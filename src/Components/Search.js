ef47e609cebb7f67329f559ffa8b78c1b8598c33
Mika <Micaelayalandino@gmail.com>
2022-04-12 21:14:30 -0300

    agrego estilado en search

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,3 +1,5 @@
+import '../Styles/Search.scss'
+
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 
@@ -7,18 +9,21 @@ const Search = () => {
     const [searchResultados, setSearchResultados] = useState([])
 
     useEffect(() => {
-        fetch(`https://api.themoviedb.org/3/search/multi?query=${params.title}`)
+        fetch(`https://api.themoviedb.org/3/search/multi?api_key=e25295b648c3ef3c3b8c8c319786e4fb&query=${params.title}`)
         .then(res => res.json()
         .then(data => {
-            setSearchResultados(data)
+            setSearchResultados(data.results)
         }))
     }, [])
 
     return (
-        <div>
+        <div className="search">
             <h1>Resultados para: </h1>
             {searchResultados.map(resultado => (
-                <h1 key={resultado.id}>{resultado.name}</h1>
+                <div>
+                    <img src={resultado.poster_path} alt={resultado.poster_path}></img>
+                    <h2 key={resultado.id}>{resultado.name}</h2>
+                </div>
             ))
             }
 
@@ -28,4 +33,3 @@ const Search = () => {
 } 
 
 export default Search;
-
