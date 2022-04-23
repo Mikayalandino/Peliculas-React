0d59e80e9a645cef63200b37ad734d63b4bde33d
Mika <Micaelayalandino@gmail.com>
2022-04-22 22:57:53 -0300

    arreglo el estilado de search

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,33 +1,32 @@
-import "../Styles/Search.scss";
-import "../Styles/Paginado.scss"
+import "../Styles/HomeTendencias.scss";
+import "../Styles/Paginado.scss";
 
 import { urlApi, apiKey, lenguageEs } from "../Variables Auxiliares/auxiliares";
-import BrokenImg from "../Images/BrokenImg.png"
+import BrokenImg from "../Images/BrokenImg.png";
 import Paginacion from "./Paginacion";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 
-import CircularProgress from '@mui/material/CircularProgress';
-import Box from '@mui/material/Box'
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
 
-const Search = (searchResultado) => {
+const Search = () => {
   const params = useParams();
   const [searchResultados, setSearchResultados] = useState([]);
-  const [isLoading, setIsLoading] = useState(false)
-  const [dataPaginado, setDataPaginado] = useState({})
-
+  const [isLoading, setIsLoading] = useState(false);
+  const [dataPaginado, setDataPaginado] = useState({});
 
   useEffect(() => {
-    setIsLoading(true)
+    setIsLoading(true);
     fetch(
       `${urlApi}/search/multi?${apiKey}&query=${params.title}&${lenguageEs}/1`
     ).then((res) =>
       res.json().then((data) => {
         setSearchResultados(data.results);
-        setIsLoading(false)
-        setDataPaginado(data)
+        setIsLoading(false);
+        setDataPaginado(data);
       })
     );
   }, [params.title]);
@@ -42,33 +41,42 @@ const Search = (searchResultado) => {
   };
 
   return (
-    <section>
-      {isLoading && <Box sx={{ display: 'flex' }}>
-        <CircularProgress />
-      </Box>}
-      <div>
-        <h1 className="title">Resultados para:</h1>
-          {searchResultados.map((resultado) => (
-            <Link className="search-cards" to={`/${!!resultado.title ? "movie" : "tv"}/${resultado.id}/info`}
+    <div className="container-home">
+      {isLoading && (
+        <Box sx={{ display: "flex" }}>
+          <CircularProgress />
+        </Box>
+      )}
+      <div className="container-home-card">
+        <div className="peliculas-tendencia">
+          <h1 className="title">Resultados para:</h1>
+        </div>
+      </div>
+      <div className="cards-tendencias">
+        {searchResultados.map((resultado) => (
+          <div>
+          <Link
+            to={`/${!!resultado.title ? "movie" : "tv"}/${resultado.id}/info`}
             key={resultado.id}>
-            <div>
-              {resultado.poster_path ? <img
+            {resultado.poster_path ? (
+              <img
                 src={`https://image.tmdb.org/t/p/w300/${resultado.poster_path}`}
                 alt={resultado.poster_path}
                 onMouseEnter={handleMouseEnter}
                 onMouseLeave={handleMouseLeave}
-                /> : <img className="broken-img" src={BrokenImg}  alt="Imagen rota"/>
-              }   
-              <h3>{resultado.title}</h3>
+              />
+            ) : (
+              <img className="broken-img" src={BrokenImg} alt="Imagen rota" />
+            )}
+             </Link>
+            <div className="peliculas-tendencia">
+              <h3 className="search-title">{resultado.title}</h3>
             </div>
-            </Link>
-          ))}
+            </div>
+         
+        ))}
       </div>
-
-      <Paginacion 
-      />
-
-    </section>
+    </div>
   );
 };
 
